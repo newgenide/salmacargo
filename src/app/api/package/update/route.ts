b5cb95d027698ede34445a1ae685a0da0b21d066
Godwin Idemudia <godwinide@gmail.com>
2025-03-10 01:59:24 +0100

    new

--- src/app/api/package/update/route.ts
+++ src/app/api/package/update/route.ts
@@ -1,12 +1,3 @@
-
-export const config = { runtime: 'nodejs' };
-
-import crypto from 'crypto';
-// Bypass the type check by casting global to any
-if (!(global as any).crypto) {
-  (global as any).crypto = crypto;
-}
-
 import { connectDb, geocodeAddress } from "@/utils";
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
