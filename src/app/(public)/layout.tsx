09624ba1a493fdada6ba2753a7e7885c068a5a9d
Godwin Idemudia <godwinide@gmail.com>
2025-04-06 12:42:00 +0100

    add livechat

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -22,15 +22,7 @@ export default async function PublicRootLayout({
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              window._smartsupp = window._smartsupp || {};
-              window._smartsupp.key = '8ddfe529d6f36eaf2cf131a3fee7abb737afa36b';
-              window.smartsupp||(function(d) {
-                var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-                s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-                c.type='text/javascript';c.charset='utf-8';c.async=true;
-                c.src='https://www.smartsuppchat.com/loader.js?';
-                s.parentNode.insertBefore(c,s);
-              })(document);
+              <script src="//code.jivosite.com/widget/HKQT2r7BW2" async></script>
             `,
           }}
         />
