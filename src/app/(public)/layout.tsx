8a56979a9323ed05125f2b98f392d54ddd4063f8
Godwin Idemudia <godwinide@gmail.com>
2025-04-08 14:26:51 +0100

    NEW

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -17,7 +17,26 @@ export default async function PublicRootLayout({
     {children}
     <Footer/>
 
-    <Script src="//code.tidio.co/maqsuudmfh44liytsxxhmdvpjb7s5nqe.js" async></Script>
+    <Script id="smartsupp-script" strategy="afterInteractive">
+          {`
+            var _smartsupp = _smartsupp || {};
+            _smartsupp.key = 'f4c774cbffd451b8d00815ace4ace2ea9fa425f2';
+            window.smartsupp||(function(d) {
+              var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+              s=d.getElementsByTagName('script')[0];
+              c=d.createElement('script');
+              c.type='text/javascript';c.charset='utf-8';c.async=true;
+              c.src='https://www.smartsuppchat.com/loader.js?';
+              s.parentNode.insertBefore(c,s);
+            })(document);
+          `}
+        </Script>
+        <noscript>
+          Powered by{' '}
+          <a href="https://www.smartsupp.com" target="_blank" rel="noopener noreferrer">
+            Smartsupp
+          </a>
+        </noscript>
     </>
   )
 }
